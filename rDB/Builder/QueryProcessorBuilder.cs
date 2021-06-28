79ddddc3d2bd14823e334aff75efc08594e5f97a
Simon Pettersson <rickebo@rickebo.com>
2021-06-28 14:43:05 +0200

    Added query processor builder

---
+++ rDB/Builder/QueryProcessorBuilder.cs
@@ -0,0 +1,30 @@
+﻿using SqlKata;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rDB.Builder
+{
+    public class QueryProcessorBuilder
+    {
+        private readonly List<Func<Query, Query>> _processors = new List<Func<Query, Query>>();
+
+        internal QueryProcessorBuilder()
+        {
+            
+        }
+
+        public QueryProcessorBuilder Where(QueryProcessor processor) =>
+            Where(processor.Function);
+
+        public QueryProcessorBuilder Where(Func<Query, Query> processor)
+        {
+            _processors.Add(processor);
+            return this;
+        }
+
+        public QueryProcessor Build() => 
+            new QueryProcessor(_processors.ToArray());
+    }
+}
